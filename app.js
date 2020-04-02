405a28f79881fb920435feaf6c682f060204f8bf
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-02 09:54:46 -0600

    add additional packages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 console.log('Welcome to my Express Web Server');    //not running on the front end
 
@@ -9,6 +10,22 @@ console.log('Welcome to my Express Web Server');    //not running on the front e
 //    res.send('Hello World!');
 //   });
 
-    app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get('/contacts', function (req, res) {
+    res.send('Hello World!')
+  });
+
+app.post('/contacts', function (req, res) {
+    res.send('Got a POST request')
+  });
+
+app.put('/contacts', function (req, res) {
+    res.send('Got a PUT request at /user')
+  });
+
+app.delete('/contacts', function (req, res) {
+    res.send('Got a DELETE request at /user')
+  });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
