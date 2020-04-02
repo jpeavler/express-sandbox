1ee3e952f6f40ba11fe1df33b850ef6bd742d4f5
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-02 15:23:31 -0600

    update CRUD functions

--- app.js
+++ app.js
@@ -11,21 +11,65 @@ console.log('Welcome to my Express Web Server');    //not running on the front e
 //   });
 
 app.use(express.static('public'));
+app.use(express.json());
 
-app.get('/contacts', function (req, res) {
-    res.send('Hello World!')
+let index = 1;
+const tvShows = [
+    {
+        Title: "Columbo",
+        Seasons: 10,
+        Actor: "Peter Falk",
+        ID: 1
+    }
+];
+
+app.get('/TVshows', function (req, res) {
+    res.send(tvShows);
   });
 
-app.post('/contacts', function (req, res) {
-    res.send('Got a POST request')
+app.post('/TVshows', function (req, res) {
+    const body = req.body;
+    tvShows.push(body);
+    body.ID = ++index;
+    res.send(body);
   });
 
-app.put('/contacts', function (req, res) {
-    res.send('Got a PUT request at /user')
+app.put('/TVshows', function (req, res) {
+    const foundIndex = tvShows.findIndex((element) => req.body.ID === element.ID);
+    let result;
+    if(foundIndex == -1){
+        tvShows.push(req.body);
+        req.body.ID = ++index;
+        result = {notice: "No object with that Id exists, created new object instead." };
+    }else{
+        tvShows.fill(req.body, foundIndex, foundIndex+1);
+        result = tvShows[foundIndex];
+    }
+    res.send(result);
   });
 
-app.delete('/contacts', function (req, res) {
-    res.send('Got a DELETE request at /user')
+app.patch('/TVshows', function (req, res) {
+    const foundIndex = tvShows.findIndex((element) => req.body.ID === element.ID);
+    let result;
+    if(foundIndex == -1){
+        result = {error: "No object with that Id exists." };
+    }else{
+        tvShows.fill(req.body, foundIndex, foundIndex+1);   //still need to change
+        result = tvShows[foundIndex];
+    }
+    res.send(result);
+  });
+
+app.delete('/TVshows', function (req, res) {
+    const foundIndex = tvShows.findIndex((element) => req.body.ID === element.ID);
+    let result;
+    if(foundIndex == -1){
+        result = {error: "Id not found"};
+    }else{
+        result = tvShows[foundIndex];
+        tvShows.splice(foundIndex,1);
+    }
+    res.send(result)
   });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
